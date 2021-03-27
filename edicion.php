7e29ffbd7fc8920ffcf5ada42080151b0e6e83b4
juananguillo <56482870+juananguillo@users.noreply.github.com>
2021-03-27 19:59:13 +0100

    editor

--- edicion.php
+++ edicion.php
@@ -10,13 +10,13 @@
     <body>
     <div class="text-center container">
    
-    <input type="text" class="display-4 text-center mb-5 mt-5" placeholder="Titulo del Capitulo">
+    <input type="text" class="text-center mb-5 mt-5" placeholder="Titulo del Capitulo" width="208">
     <div class="text-left">
     <div class="text-right">Ultimo guardado automatico</div>
-    <input  class="btn btn-secondary " type="submit" id="guardar" value="Guardado manual">
-    <input  class="btn btn-secondary " type="submit" id="publicar" value="Publicar capitulo">
+    <input  class="btn btn-primary " type="submit" id="guardar" value="Guardado manual">
+    <input  class="btn btn-primary " type="submit" id="publicar" value="Publicar capitulo">
     </div>
-      <textarea  id="textarea" rows="20" name="textarea">
+      <textarea  id="textarea" rows="30" name="textarea">
       <?php echo "Hola"; ?>
       </textarea>
   
